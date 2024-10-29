764bde35b53aeaf5163acfbc6d0b17937a798272
Kat <katavamp@gmail.com>
2024-10-28 19:38:42 -0500

    works and testimonial

---
+++ src/components/Testimonials/Testimonial.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Testimonial.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { Pagination } from "swiper";
+import "swiper/css/pagination";
+import profilePic1 from "../../img/profile1.jfif";
+
+
+const Testimonial = () => {
+  const clients = [
+    {
+      img: profilePic1,
+      review:
+"Katalin has been instrumental in the growth of the IT team at Startup Lady. During her time here she has provided numerous IT solutions and support for our webpage & membership portal that ultimately ensure the success of our membership launch. In-person Katalin is friendly, easy to work with, has good communication skills, and takes initiative and responsibilities for her work. I wouldn't hesitate to recommend her to anyone. - Amee Xu, Startuplady Japan"    },
+  
+ 
+  ];
+
+  return (
+    <div className="t-wrapper" id="testimonial">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>from me...</span>
+      <div className="blur t-blur1" style={{ background: "var(--purple)" }}></div>
+      <div className="blur t-blur2" style={{ background: "skyblue" }}></div>
+
+      </div>
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonial">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonial;
